2b2c1c9d9a1f1dd3ea9692519fe865a80b145039
boogiebouncemma <bogusia.kedziora@gmail.com>
2022-04-30 15:44:55 +0200

    paralax effect

--- script.js
+++ script.js
@@ -3,15 +3,22 @@ let imgRightWoods = document.getElementById("img_right_woods")
 let imgMountains = document.getElementById("img_mountains")
 let imgRoad = document.getElementById("img_road")
 let tPortfolio = document.getElementById("t_portfolio")
+let bScrollDown = document.getElementById("scroll_down")
 
 window.addEventListener('scroll', function(){
     var value = window.scrollY;
     // imgLeftWoods.style.left = -value * 0.5 + 'px';
     // imgRightWoods.style.right = -value * 0.5 + 'px';
 
-    imgMountains.style.transform = "scale("+ (1+ Math.pow(value,1/4)*0.1)  +")";
+    // imgMountains.style.transform = "scale("+ (1+ Math.pow(value,1/4)*0.1)  +")";
+    imgMountains.style.transform = "scale("+ (1+ value*0.005)  +")";
+    imgRoad.style.transform = "scale("+ (1+ value*0.004)  +")";
+    imgLeftWoods.style.transform = "scale("+ (1+ value*0.004)  +")";
+    imgRightWoods.style.transform = "scale("+ (1+ value*0.004)  +")";
     tPortfolio.style.top = -value * 1.5 + 'px';
     console.log(value)
-    console.log(1+ Math.pow(value,1/4)*0.1)
-
   })
+
+bScrollDown.addEventListener("click", function () {
+    window.scrollTo(0,180);
+})
