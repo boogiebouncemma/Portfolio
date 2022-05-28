7c67467b08d695d30c20040c6bbdb082287af88b
boogiebouncemma <bogusia.kedziora@gmail.com>
2022-05-28 17:02:54 +0200

    navbar, title animation, background

--- script.js
+++ script.js
@@ -11,14 +11,37 @@ window.addEventListener('scroll', function(){
     // imgRightWoods.style.right = -value * 0.5 + 'px';
 
     // imgMountains.style.transform = "scale("+ (1+ Math.pow(value,1/4)*0.1)  +")";
-    imgMountains.style.transform = "scale("+ (1+ value*0.005)  +")";
-    imgRoad.style.transform = "scale("+ (1+ value*0.004)  +")";
-    imgLeftWoods.style.transform = "scale("+ (1+ value*0.004)  +")";
-    imgRightWoods.style.transform = "scale("+ (1+ value*0.004)  +")";
-    tPortfolio.style.top = -value * 1.5 + 'px';
-    console.log(value)
+    imgMountains.style.transform = "scale("+ (1+ value*0.003)  +")";
+    imgMountains.style.bottom = value * 0.2 + 'px';
+    imgRoad.style.transform = "scale("+ (1+ value*0.002)  +")";
+    imgRoad.style.bottom = value * 0.1 + 'px';
+    imgLeftWoods.style.transform = "scale("+ (1+ value*0.002)  +")";
+    imgLeftWoods.style.bottom = value * 0.1 + 'px';
+    imgRightWoods.style.transform = "scale("+ (1+ value*0.002)  +")";
+    imgRightWoods.style.bottom = value * 0.1 + 'px';
+    tPortfolio.style.top = value * 1.5 + 'px';
+
   })
 
 bScrollDown.addEventListener("click", function () {
     window.scrollTo(0,180);
 })
+
+/* navbar */
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {navbarFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("header_text");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function navbarFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
